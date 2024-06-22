bcaa9ded128465a2bc333dbb9e33c301d139dea4
emirsassan <endwse@gmail.com>
2024-06-22 11:36:16 +0300

    use layouts in index route

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,46 +95,16 @@ import { useState } from "react"
 import { Label } from "@/components/ui/label"
 import { DialogClose } from "@radix-ui/react-dialog"
 import { GetServerSidePropsContext } from "next"
+import Layout from "@/components/layout"
 
 interface ShowPassword {
   [key: number]: boolean;
 }
 
-interface ILogin {
-  username: string;
-  password: string;
-  url: string;
-} 
-
 export default function Dashboard({ data }: any) {
-  const [passwords, setPasswords] = useState([
-    {
-      id: 1,
-      website: "www.example.com",
-      username: "johndoe",
-      password: "mypassword123",
-    },
-    {
-      id: 2,
-      website: "www.acme.com",
-      username: "janedoe",
-      password: "secretpassword",
-    },
-    {
-      id: 3,
-      website: "www.github.com",
-      username: "devuser",
-      password: "opensesame",
-    },
-  ])
-  const [newPass, setNewPass] = useState<any>({})
   const [searchTerm, setSearchTerm] = useState("")
   const [showPassword, setShowPassword] = useState<ShowPassword>({})
-  const filteredPasswords = passwords.filter(
-    (password) =>
-      password.website.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      password.username.toLowerCase().includes(searchTerm.toLowerCase()),
-  )
+
   const toggleShowPassword = (id: any) => {
     setShowPassword((prevState) => ({
       ...prevState,
@@ -146,274 +116,79 @@ export default function Dashboard({ data }: any) {
     navigator.clipboard.writeText(text)
   }
 
-  const createNewLogin = async () => {
-    if (!newPass) {
-      alert("Please fill all fields")
-      return
-    }
-
-    await fetch("http://localhost:3001/api/logins/create", {
-      method: "POST",
-      body: JSON.stringify(newPass),
-    })
-  }
-
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-        <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-          <Link
-            href="/"
-            className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-          >
-            <Cuboid className="h-4 w-4 transition-all group-hover:scale-110" />
-            <span className="sr-only">Toka</span>
-          </Link>
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="/"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                >
-                  <Home className="h-5 w-5" />
-                  <span className="sr-only">Dashboard</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right">Dashboard</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="/generator"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                >
-                  <Book className="h-5 w-5" />
-                  <span className="sr-only">Generator</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right">Generator</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </nav>
-        <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="/"
-                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                >
-                  <Settings className="h-5 w-5" />
-                  <span className="sr-only">Settings</span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent side="right">Settings</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </nav>
-      </aside>
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button size="icon" variant="outline" className="sm:hidden">
-                <PanelLeft className="h-5 w-5" />
-                <span className="sr-only">Toggle Menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="left" className="sm:max-w-xs">
-              <nav className="grid gap-6 text-lg font-medium">
-                <Link
-                  href="/"
-                  className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-                >
-                  <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
-                  <span className="sr-only">Toka</span>
-                </Link>
-                <Link
-                  href="/"
-                  className="flex items-center gap-4 px-2.5 text-foreground"
-                >
-                  <Home className="h-5 w-5" />
-                  Dashboard
-                </Link>
-                <Link
-                  href="/"
-                  className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                >
-                  <Book className="h-5 w-5" />
-                  Passwords
-                </Link>
-              </nav>
-            </SheetContent>
-          </Sheet>
-          <Breadcrumb className="hidden md:flex">
-            <BreadcrumbList>
-              <BreadcrumbItem>
-                <BreadcrumbLink asChild>
-                  <Link href="/">Dashboard</Link>
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                <BreadcrumbLink asChild>
-                  <BreadcrumbPage>General</BreadcrumbPage>
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-          <div className="relative ml-auto flex-1 md:grow-0">
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button>
-                  Add Login
-                </Button>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                  <DialogTitle>Add new Login</DialogTitle>
-                  <DialogDescription>
-                    Fill inputs and Click save when you're done.
-                  </DialogDescription>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                      Username
-                    </Label>
-                    <Input
-                      id="username"
-                      className="col-span-3"
-                      required
-                      onChange={(e) => setNewPass({ ...newPass, username: e.target.value })}
-                    />
-                  </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="password" className="text-right">
-                      Password
-                    </Label>
-                    <Input
-                      id="password"
-                      className="col-span-3"
-                      required
-                      onChange={(e) => setNewPass({ ...newPass, password: e.target.value })}
-                    />
-                  </div>
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="website" className="text-right">
-                      Website
-                    </Label>
-                    <Input
-                      id="website"
-                      className="col-span-3"
-                      required
-                      onChange={(e) => setNewPass({ ...newPass, url: e.target.value })}
-                    />
-                  </div>
-                </div>
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button type="submit" onClick={async () => await createNewLogin()}>Save changes</Button>
-                  </DialogClose>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-          </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="overflow-hidden rounded-full"
-              >
-                <Image
-                  src="https://github.com/emirsassan.png"
-                  width={36}
-                  height={36}
-                  alt="Avatar"
-                  className="overflow-hidden rounded-full"
-                />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </header>
-        <div className="flex flex-col text-foreground overflow-y-auto">
-          <main className="flex-1 p-6">
-            <div className="relative flex-1 md:grow-0 ml-auto mb-6">
-              <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="search"
-                placeholder="Search passwords..."
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-              />
-            </div>
-            <div className="overflow-auto">
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Website</TableHead>
-                    <TableHead>Username</TableHead>
-                    <TableHead>Password</TableHead>
-                    <TableHead className="text-right">Actions</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {data.data.map((password: any) => (
-                    <TableRow key={password.id}>
-                      <TableCell>{password.url}</TableCell>
-                      <TableCell>{password.username}</TableCell>
-                      <TableCell>
-                        {showPassword[password.id]
-                          ? password.password
-                          : "\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022"}
-                      </TableCell>
-                      <TableCell className="text-right">
-                        <Button variant="ghost" size="icon" onClick={() => toggleShowPassword(password.id)}>
-                          <Eye className="h-5 w-5" />
-                        </Button>
-                        <Button variant="ghost" size="icon" onClick={() => copyToClipboard(password.password)}>
-                          <Clipboard className="h-5 w-5" />
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
-          </main>
-        </div>
+    <Layout>
+      <div className="relative flex-1 md:grow-0 ml-auto mb-6">
+        <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
+        <Input
+          type="search"
+          placeholder="Search passwords..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+        />
       </div>
-    </div>
+      <div className="overflow-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Website</TableHead>
+              <TableHead>Username</TableHead>
+              <TableHead>Password</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.data.map((password: any) => (
+              <TableRow key={password.id}>
+                <TableCell>{password.url}</TableCell>
+                <TableCell>{password.username}</TableCell>
+                <TableCell>
+                  {showPassword[password.id]
+                    ? password.password
+                    : "\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022\u2022"}
+                </TableCell>
+                <TableCell className="text-right">
+                  <Button variant="ghost" size="icon" onClick={() => toggleShowPassword(password.id)}>
+                    <Eye className="h-5 w-5" />
+                  </Button>
+                  <Button variant="ghost" size="icon" onClick={() => copyToClipboard(password.password)}>
+                    <Clipboard className="h-5 w-5" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </Layout>
   )
 }
 
+// Idk why this shit dont works
+/*
+Dashboard.getLayout = function (page: React.ReactNode) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
+}
+*/
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const headers: { [token: string]: string } = {};
 
   if (context.req.headers.cookie) {
     headers.cookie = context.req.headers.cookie;
   }
-  
+
   const res = await fetch("http://localhost:3000/logins/all", {
     method: 'GET',
     headers
   })
 
   const data = await res.json()
-  
+
   return {
     props: {
       data
